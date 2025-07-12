8ee99f1e08916cc74f4e60346f787926444ef2f1
Bill Dean <742designs@gmail.com>
2025-07-12 10:57:04 -0400

    Adding latest updates.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,38 +1,54 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+import GithubContext from '../../context/github/githubContext';
 
-const Search = ({searchUsers, showClear, clearUsers, setAlert  }) => {
-    const [text, setText] = useState('');
+const Search = () => {
+  const alertContext = useContext(AlertContext);
+  const githubContext = useContext(GithubContext);
 
-    const onSubmit = e => {
-        e.preventDefault();
-        if (text === '') {
-            setAlert('Please enter something', 'light');
-        } else {
-            searchUsers(text);
-            setText('');
-        }
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      alertContext.setAlert('Please enter something', 'light');
+    } else {
+      githubContext.searchUsers(text);
+      setText('');
     }
+  };
 
-    const onChange = (e) => setText(e.target.value)
+  const onChange = (e) => setText(e.target.value);
 
-    return (
-        <div>
-            <form onSubmit={onSubmit} className="form">
-                <input type="text" name="text" id="" placeholder='Search Users' value={text} onChange={onChange} />
-                <input type="submit" value="Search" className="btn btn-dark btn-block"/>
-            </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          id=""
+          placeholder="Search Users"
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
 
-            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
-        </div>
-    )
-}
-
-Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+          type="button"
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
 };
 
-export default Search
+export default Search;
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 
-const Users = ({ users, loading }) => {
-  if(loading) {
-    return <Spinner />
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
+
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
+  if (loading) {
+    return <Spinner />;
   } else {
     return (
       <div style={userStyle}>
-        {users.map(user => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired,
-}
-
-const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
-}
-
-export default Users
+export default Users;
