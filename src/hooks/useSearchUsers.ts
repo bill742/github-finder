ec4834ce6f654622896745059bbfd30c2b4a9cbd
Bill Dean <742designs@gmail.com>
2026-03-24 19:17:37 -0400

    Add pagination to searchUsers query.

--- src/hooks/useSearchUsers.ts
+++ src/hooks/useSearchUsers.ts
@@ -2,9 +2,9 @@ import { useQuery } from '@tanstack/react-query';
 
 import { searchUsers } from '../api/github';
 
-export const useSearchUsers = (text: string) =>
+export const useSearchUsers = (text: string, page: number) =>
   useQuery({
     enabled: !!text,
-    queryFn: () => searchUsers(text),
-    queryKey: ['searchUsers', text],
+    queryFn: () => searchUsers(text, page),
+    queryKey: ['searchUsers', text, page],
   });
